c646a5b17b8aa7d415f461c5605b98e4465a419a
Robin Scavo <sirscavo@gmail.com>
2021-01-16 22:34:15 -0700

    pregame model done

---
+++ frontend/src/components/HighScoreDisplay/HighScoreDisplay.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import './HighScoreDisplay.css'
+
+const HighScoreDisplay = () => {
+    const data  = [
+        {rank: 1, name: 'Robin Scavo', score: 477},
+        {rank: 2, name: 'Angela Scavo', score: 467},
+        {rank: 3, name: 'Tom Scavo', score: 460},
+        {rank: 4, name: 'Elain Bodurtha', score: 451},
+        {rank: 5, name: 'Michelle Sever', score: 449},
+        {rank: 6, name: 'Dawsen Sever', score: 439},
+        {rank: 7, name: 'Shelby Sever', score: 428},
+        {rank: 8, name: 'Jeremy Sever', score: 422},
+        {rank: 9, name: 'Oakley Scavo', score: 409},
+        {rank: 10, name: 'Sebastian Scavo', score: 390},
+        {rank: 11, name: 'Alice Berglund', score: 388},
+        {rank: 12, name: 'Mary Russell', score: 311},
+
+    ]
+
+    const coinShine = () => Math.floor((Math.random() * 12) + 9)
+
+    return (
+        <div className='highScoreDiv'>
+            <h1 className='highScoreText'>High Scores</h1>
+            <table className='highScoreTable'>
+                {data.map(user => (
+                    <tr className='tableRow'>
+                        <th className='rankColumn'>
+                            <div className='rankBling' style={{animation: 'TransitioningBackground 1s linear infinite;'}}>
+                                {user.rank}
+                            </div>
+                        </th>
+                        <th className='userColumn'>{user.name}</th>
+                        <th className='scoreColumn'>{user.score}</th>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    )
+}
+
+
+
+export default HighScoreDisplay;
--- frontend/src/components/Modals/InstructionModal.js
+++ frontend/src/components/Modals/InstructionModal.js
@@ -3,9 +3,11 @@ import { Modal } from '../../ModalContext/Modal';
 
 // import SignUpForm from '../SignupFormPage/SignUpFormPage';
 import LoginFormModal from '../Modals/LoginFormModal';
+import HighScoreDisplay from '../../Components/HighScoreDisplay/HighScoreDisplay'
 import KarenContext from '../../KarenContext'
 
 import './LogInFormModal.css'
+import './InstructionModel.css'
 import SignUpModal from './SignUpModal';
 
 function InstructionModel(props) {
@@ -14,41 +16,95 @@ function InstructionModel(props) {
   const [showLoginModal, setShowLoginModal] = useState(false);
 
   return (
-    <>
-        {/* ZenMode */}
-        <button>Zen Mode</button>
+    <div className='instructionDiv'>
+      <div className='allModeDiv'>
+        <div className='upperDiv'>
+          <div className='playDiv'>
+            <h1 className='greetingText'>Play as a guest!</h1>
+            <h1 className='downArrow'><span>➛</span></h1>
+          </div>
+          {/* <h1 className='greetingText logSignText'>Log in or sign up!<span className='arrow arrowLog'>➛</span></h1> */}
+          <div className='logArrowText'>
+                <h1 className='greetingText logSignText'>Log in or sign up!</h1>
+                <h1 className='arrow arrowLog'>➛</h1>
+              </div>
+          <div className='optionDiv'>
+            {/* Log In */}
+            <button className='logStateButton' onClick={() => {
+              setShowLoginModal(true)
+            }}>Log In</button>
+            {showLoginModal && (
+              <Modal >
+                  <LoginFormModal closeModal={props.closeModal}/>
+              </Modal>
+            )}
 
-        {/* BattleMode */}
-        <button onClick={() => {
-          props.closeModal();
-          values.startGame();
-        }}>Battle Mode</button>
+            {/* Sign Up */}
+            <button className='logStateButton' onClick={() => {
+              setShowSignUpModal(true)
+            }}>Sign Up</button>
+            {showSignUpModal && (
+              <Modal >
+                  <SignUpModal closeModal={props.closeModal}/>
+              </Modal>
+            )}
+          </div>
+        </div>
 
-        {/* Demo mode */}
-        <button>Demo Mode</button>
+        <div className='lowerDiv'>
+          <div className='modeSelectDiv'>
+            <div className='modeDiv'>
+              <div className='buttonDiv'>
+                <h1 className='titleText'>Zen Mode</h1>
+                <button className='goldButton zen' >Zen</button>
+              </div>
+              <div className='arrowText'>
+                <h1 className='arrow'>➛</h1>
+                <h1 className='modeText'>Just click the balls. Relax.</h1>
+              </div>
+            </div>
 
-        {/* Log In */}
-        <button onClick={() => {
-          setShowLoginModal(true)
-        }}>Log In</button>
-        {showLoginModal && (
-          <Modal >
-              <LoginFormModal closeModal={props.closeModal}/>
-          </Modal>
-        )}
+            {/* BattleMode */}
+            <div className='modeDiv'>
+              <div className='buttonDiv'>
+                <h1 className='titleText'>Battle Mode</h1>
+                <button  className='goldButton battle' onClick={() => {
+                  props.closeModal();
+                  values.startGame();
+                }}>Battle</button>
+              </div>
+              <div className='arrowText'>
+                <h1 className='arrow'>➛</h1>
+                <h1 className='modeText'>Compete aginst the best!</h1>
+              </div>
+            </div>
 
-        {/* Sign Up */}
-        <button onClick={() => {
-          setShowSignUpModal(true)
-        }}>Sign Up</button>
-        {showSignUpModal && (
-          <Modal >
-              <SignUpModal closeModal={props.closeModal}/>
-          </Modal>
-        )}
-    </>
+            {/* Demo mode */}
+            <div className='modeDiv'>
+              <div className='buttonDiv'>
+                <h1 className='titleText'>Demo Mode</h1>
+                <button className='goldButton demo' >Demo</button>
+              </div>
+              <div className='arrowText'>
+                <h1 className='arrow'>➛</h1>
+                <h1 className='modeText'>A sampler of each round.</h1>
+              </div>
+            </div>
+
+          </div>
+          {/* ZenMode */}
+          <HighScoreDisplay />
+          {/* <div className='topTen'>Top Ten</div> */}
+        </div>
+      </div>
+    </div>
+
+
+
 )
 }
+
+
 
 export default InstructionModel;
 
