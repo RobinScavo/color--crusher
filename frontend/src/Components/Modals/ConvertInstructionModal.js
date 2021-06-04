642e620d90f8c67b67fd5f23dbeab50b980d2b8f
Robin Scavo <sirscavo@gmail.com>
2021-06-04 11:43:00 -0600

    convert instruction div

--- frontend/src/Components/Modals/ConvertInstructionModal.js
+++ frontend/src/Components/Modals/ConvertInstructionModal.js
@@ -1,17 +1,54 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 
-import ColorContext from '../../ColorContext'
+import ColorContext from '../../ColorContext';
+
+import './BattleInstructionModal.css';
+import './ConvertInstructionModal.css';
 
 
 const ConvertInstructionModal = () => {
     const values = useContext(ColorContext);
 
     return (
-        <button  className='silverButton' onClick={() => {
-            values.toggleConvertInstructionModal();
-            values.toggleStartConvert();
-            values.startGame();
-          }}>Got it!</button>
+        <div className='convertInstructionDiv'>
+
+            <div className='explanationDiv'><span className='defSpan'>HSL (Hue, Saturation, Light)</span><br/>
+                While RGB and Hex values use proportions
+                of red, green and blue, HSL is based on color wheel position which is
+                useful for finding color complements. Use the
+                <span className='expSpan'> Color Converter </span>
+                to convert your color values or find their compliments.
+            </div>
+
+            <div className='wheelDiv'>
+                <div className='wheel'>
+                    <h2 className='wheelLabel'>COMPLIMENT</h2>
+                    <div className='wheelImage' alt='color wheel'></div>
+                    <div className='wheelOverlayOne' alt='first overlay'></div>
+                    <h3 className='imageCredit'
+                        onClick={() => window.open('https://contentwriters.com/blog/psychology-color-content-marketing/')}>
+                            Image credit: Deborah K.
+                    </h3>
+                </div>
+                <div className='wheel'>
+                    <div className='wheelImage'></div>
+                    <div className='wheelOverlayTwo'></div>
+                    <h2 className='wheelLabel triadicLabel'>TRIADIC</h2>
+                </div>
+                <div className='wheel'>
+                    <h2 className='wheelLabel'>ANALOGOUS</h2>
+                    <div className='wheelImage'></div>
+                    <div className='wheelOverlayThree'></div>
+                    <h2 className='wheelLabel'>SPLIT COMPLIMENT</h2>
+                </div>
+            </div>
+
+            <button  className='gotItButton' onClick={() => {
+                values.toggleConvertInstructionModal();
+                values.toggleStartConvert();
+                values.startGame();
+              }}>Got it!</button>
+        </div>
     )
 }
 
